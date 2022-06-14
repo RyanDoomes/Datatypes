facc0242db042203181f7591f576f14685d30ebd
RyanDoomes <101357291+RyanDoomes@users.noreply.github.com>
2022-06-14 13:16:05 -0700

    Completed Program

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+
+        int weeks;
+
+        System.out.println("Enter a number of weeks: ");
+        weeks = scan.nextInt();
+
+        System.out.println("hours: " + (weeks)*168);
+        System.out.println("minutes: " + (weeks)*10080);
+        System.out.println("seconds: " + (weeks)*604800);
+
+
+    }
+}
+
