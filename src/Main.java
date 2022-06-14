36d5c8edca8e19d2c0379f2e214107b6ecdeb84b
RyanDoomes <101357291+RyanDoomes@users.noreply.github.com>
2022-06-14 13:32:36 -0700

    adding "days"
    commit#2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Enter a number of weeks: ");
         weeks = scan.nextInt();
 
+        System.out.println("days: " + (weeks)*7);
         System.out.println("hours: " + (weeks)*168);
         System.out.println("minutes: " + (weeks)*10080);
         System.out.println("seconds: " + (weeks)*604800);
